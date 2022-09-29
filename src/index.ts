01b216336543b43c81de935f46631f5b30d5fb8b
heyu <1395294694@qq.com>
2022-09-29 21:14:23 +0800

    feat: 生成组件描述

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,50 @@
 // / <reference path="./type.d.ts" />
+import { resolve } from 'path';
+import fs from 'fs-extra';
 import parse from './parse/index'
-import {Prop} from './schema/type'
 
-export default async function run(componentAbsolutePath: string, args: {workDir: string, tsconfigFileName?: string}): Promise<{ props: Prop[] }> {
+export default async function run(componentAbsolutePath: string, args: {workDir: string, tsconfigFileName?: string}): Promise<string> {
     const componentDocs = parse(componentAbsolutePath, args)
-    return componentDocs[0]
+    const { componentConfig = {}, version = '0.0.0', description, name } = JSON.parse(fs.readFileSync( resolve(process.cwd(), 'package.json'), {encoding: 'utf-8'} ))
+    return JSON.stringify(
+        {
+          componentName: componentConfig.name || '',
+          packageName: name,
+          title: componentConfig.title || '',
+          iconUrl: componentConfig.iconUrl || '',
+          description,
+          docUrl:`https://unpkg.com/${name}@${version}/docs/index.html`,
+          version: version,
+          props: componentDocs[0] ? componentDocs[0].props: '',
+          advanced: {
+            // 组件的嵌套规则
+            nestingRule: {
+              // 父级组件白名单
+              // 业务组件必须放置在布局组件中
+              parentWhitelist: ['LayoutColumn'],
+              // 子组件白名单。
+              // 空数组则说明其他组件不能放置在该组件中
+              childWhitelist: []
+            },
+            supports: {
+              // 是否能配置样式
+              styles: true,
+              // 是否能配置校验规则
+              validation: false,
+              // 是否能配置联动规则
+              linkage: false,
+              // 支持的事件列表
+              events: ['onClick']
+            },
+            // 该组件是否是容器，容器能拥有自己的数据源
+            isContainer: false,
+            // 是否是表单组件
+            isFormControl: false,
+            // 是否是布局组件
+            isLayout: false
+          },
+        },
+        null,
+        2,
+    )
 }
